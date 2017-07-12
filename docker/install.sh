3e3572a02b129f418ced0d4e7ce9570c3a9f1f3a
Jon Jagger <jon@jaggersoft.com>
2017-07-12 11:29:52 +0100

    use long --options in install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -10,7 +10,7 @@ export CC=clang-${CLANG_VERSION}
 cd /usr/src
 
 apt-get update
-apt-get install -y wget cmake unzip
+apt-get install --yes wget cmake unzip
 
 #WAS wget http://googlemock.googlecode.com/files/gmock-1.7.0.zip
 wget http://pkgs.fedoraproject.org/repo/pkgs/gmock/gmock-${GMOCK_VERSION}.zip/073b984d8798ea1594f5e44d85b20d66/gmock-${GMOCK_VERSION}.zip
@@ -29,4 +29,4 @@ cp -rf include/gmock /usr/include
 cd /usr/src/gmock-${GMOCK_VERSION}/gtest
 mv libg* /usr/lib && cp -rf include/gtest /usr/include
 
-apt-get remove -y wget cmake unzip
+apt-get remove --yes wget cmake unzip
