59d3584bf1d72b1da81916962b41abd3e524a590
Jon Jagger <jon@jaggersoft.com>
2018-02-09 12:26:42 +0000

    COPY gmock zip file into container rather than wget'ing from URL that no longer works

--- docker/install.sh
+++ docker/install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 set -e
 
-readonly CLANG_VERSION=5.0
 readonly GMOCK_VERSION=1.7.0
-
-export CXX=clang++-${CLANG_VERSION}
-export CC=clang-${CLANG_VERSION}
 
 cd /usr/src
 
 apt-get update
 apt-get install --yes wget cmake unzip
 
-#WAS wget http://googlemock.googlecode.com/files/gmock-1.7.0.zip
-wget http://pkgs.fedoraproject.org/repo/pkgs/gmock/gmock-${GMOCK_VERSION}.zip/073b984d8798ea1594f5e44d85b20d66/gmock-${GMOCK_VERSION}.zip
 unzip gmock-${GMOCK_VERSION}.zip
 
 cd /usr/src/gmock-${GMOCK_VERSION}
@@ -27,3 +21,4 @@ mv libg* /usr/lib
 cp -rf include/gtest /usr/include
 
 apt-get remove --yes wget cmake unzip
+
