2b5f640f19c2f9ecddca64c895aada593dc7fc6c
Matthias Saft <matthias.saft@siemens.com>
2026-03-03 11:41:18 +0100

    POC to further reduce build time

--- docker/install.sh
+++ docker/install.sh
@@ -5,7 +5,6 @@ readonly GTEST_VERSION=1.17.0
 # to speed up linking : -fuse-ld=mold
 apt-get update -y
 apt-get install --yes --no-install-recommends cmake unzip mold
-apt-get clean
 
 cd /usr/src
 unzip googletest-release-${GTEST_VERSION}.zip
@@ -14,11 +13,14 @@ cmake .
 make
 
 apt-get remove --yes cmake unzip
+apt-get autoremove -y
+apt-get clean
+
 mv lib/libg* /usr/lib
 cp -rf googlemock/include/gmock /usr/include
 cp -rf googletest/include/gtest /usr/include
 
-CXX_PCH_FLAGS="-std=c++2a -pthread -O -fsanitize=leak,address"
+CXX_PCH_FLAGS="-std=c++20 -pthread -O0 -fsanitize=leak,address"
 mkdir -p /usr/local/include/gtest
 clang++ -x c++-header ${CXX_PCH_FLAGS} \
     /usr/include/gtest/gtest.h \
